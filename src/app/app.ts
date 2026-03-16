8ded114a507171c906001e4a0ccc0ac5abf0c71b
marial-garces <m.garces2103@gmail.com>
2026-03-16 15:18:45 -0400

    imports router to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('pomodoro-app');
-}
+export class App {}
