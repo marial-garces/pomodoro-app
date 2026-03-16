31fe80ca2c4241e5a23cf893dd2f0b3c8f5bb92a
marial-garces <m.garces2103@gmail.com>
2026-03-16 14:50:37 -0400

    updating the app.routes.ts :D (para que no se pierdan jajaj)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Timer } from './components/timer/timer';
+import { Settings } from './components/settings/settings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'timer', pathMatch: 'full' },
+    { path: 'timer', component: Timer },
+    { path: 'settings', component: Settings }
+];
