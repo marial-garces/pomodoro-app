ceff7d09bb67e5fe0676d3d43870ff8764d971e5
marial-garces <m.garces2103@gmail.com>
2026-03-17 14:29:46 -0400

    ts stuff in task

--- src/app/components/task-list/task-list.ts
+++ src/app/components/task-list/task-list.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TaskService } from '../../services/task';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css',
 })
 export class TaskList {
+  protected readonly taskService = inject(TaskService);
 
+  showInput = signal(false);
+  newTaskTitle = signal('');
+
+  openInput(): void {
+    this.showInput.set(true);
+  }
+  // add a task duh
+  addTask(): void {
+    const title = this.newTaskTitle().trim();
+    if(title) {
+      this.taskService.addTask(title);
+      this.newTaskTitle.set('');
+      this.showInput.set(false);
+    }
+  }
+  // cancel adding a task DUH 
+  cancelAdd(): void {
+    this.newTaskTitle.set('');
+    this.showInput.set(false);
+  }
+  // KEY PRESS enter and escape DUUUH
+  onKeydown(event: KeyboardEvent): void {
+    if (event.key === 'Enter') this.addTask();
+    if (event.key === 'Escape') this.cancelAdd();
+  }
 }
