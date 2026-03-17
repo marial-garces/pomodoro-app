6a6e9dd600457d39d32d69bb868772b94b45138a
marial-garces <m.garces2103@gmail.com>
2026-03-17 09:42:15 -0400

    timer logica stuff

--- src/app/components/timer/timer.ts
+++ src/app/components/timer/timer.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Timer as TimerService } from '../../services/timer';
 
 @Component({
   selector: 'app-timer',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './timer.css',
 })
 export class Timer {
+  // lit INJECTS the services xd
+  protected readonly timer = inject(TimerService);
+
+  //modes for the timer
+  readonly modes: { key: 'pomodoro' | 'shortBreak' | 'longBreak'; label: string }[] = [
+    { key: 'pomodoro', label: 'Pomodoro' },
+    { key: 'shortBreak', label: 'Short Break' },
+    { key: 'longBreak', label: 'Long Break' },
+  ]
+
+  // function that do the scwitches
+  switchMode(mode: 'pomodoro' | 'shortBreak' | 'longBreak'): void {
+    this.timer.switchMode(mode);
+  }
+  
+  toggleTimer(): void {
+    this.timer.toggleTimer();
+  }
+
+  resetTimer(): void {
+    this.timer.reset();
+  }
 
 }
