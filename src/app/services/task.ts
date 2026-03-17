bb8f637904bffac191bd8c0d1d631e6af3374d12
marial-garces <m.garces2103@gmail.com>
2026-03-17 12:01:53 -0400

    SERVICE TASKSS inprogresss..

---
+++ src/app/services/task.ts
@@ -0,0 +1,27 @@
+import { Injectable, signal, computed } from '@angular/core';
+import { Task } from '../models/pomodoro.models'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private tasks = signal<Task[]>([]);
+
+  readonly allTasks = computed(() => this.tasks());
+  readonly hasTasks = computed(() => this.tasks().length > 0);
+
+  // add a task
+  addTask(title: string, pomodorosEstimated: number = 1): void {
+    const newTask: Task = {
+      id: crypto.randomUUID(),
+      title,
+      completed: false,
+      createdAt: new Date(),
+      pomodorosEstimated,
+      pomodorosCompleted: 0,
+    };
+    this.tasks.update(tasks => [...tasks, newTask]);
+  }
+
+  // DELEEEEEETE
+}
